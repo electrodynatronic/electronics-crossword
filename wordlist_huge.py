76fa7da499fad89ac27ab353f50270fe56a48b9d
electrodynatronic <electrodynatronic@gmail.com>
2014-10-08 13:25:42 -0500

    Update wordlist_huge.py
    Added hints contributed by students. -CN

--- wordlist_huge.py
+++ wordlist_huge.py
@@ -9,56 +9,11 @@ word_list = ['coulomb', 'A fundamental unit of electrical charge, and is also th
     ['atto', 'A prefix in the metric system denoting a factor of one quintillionth (10-18).'], \
     ['zepto', 'A prefix in the metric system denoting a factor of one sextillionth (10-21).'], \
     ['yocto', 'A prefix in the metric system denoting a factor of one septillionth (10-24).'], \
-    ['kilo', 'define kilo'], \
-    ['Mega', 'define Mega'], \
-    ['Giga', 'define Giga'], \
-    ['Tera', 'define Tera'], \
-    ['Peta', 'define Peta'], \
     ['electron', 'define electron'], \
     ['binary', 'two states'], \
     ['transistor', 'A semiconductor device used to amplify and switch electronic signals and electrical power.'], \
     ['amplifier', 'An electronic device that increase the power of a signal.'], \
     ['inductance', 'he property of a conductor by which a change in current flowing through it "induces" (creates) a voltage (electromotive force) in both the conductor itself and in any nearby conductors.'], \
-    ['capacitance', 'define capacitance'], \
-    ['impedance', 'define impedance'], \
-    ['resistance', 'define resistance'], \
-    ['resistor', 'define resistor'], \
-    ['capacitor', 'define capacitor'], \
-    ['inductor', 'define inductor'], \
-    ['bit', 'define bit'], \
-    ['byte', 'define byte'], \
-    ['diode', 'define diode'], \
-    ['LED', 'define LED'], \
-    ['triac', 'define triac'], \
-    ['FET', 'define FET'], \
-    ['MOSFET', 'define MOSFET'], \
-    ['NPN', 'define NPN'], \
-    ['PNP', 'define PNP'], \
-    ['spectrum', 'define spectrum'], \
-    ['spectra', 'define spectra'], \
-    ['Fourier', 'define Fourier'], \
-    ['FFT', 'define FFT'], \
-    ['potentiometer', 'define potentiometer'], \
-    ['digital', 'define digital'], \
-    ['analog', 'define analog'], \
-    ['gain','define gain'], \
-    ['bridge','define bridge'], \
-    ['rectifier','define rectifier'], \
-    ['filter','define filter'], \
-    ['sawtooth','define sawtooth'], \
-    ['sine','define sine'], \
-    ['waveform','define waveform'], \
-    ['current','define current'], \
-    ['Kirchhoff','define Kirchhoff'], \
-    ['Faraday','define Faraday'], \
-    ['farad','define farad'], \
-    ['Norton','define Norton'], \
-    ['Thevenin','define Thevenin'], \
-    ['cable','define cable'], \
-    ['coaxial','define coaxial'], \
-    ['Ohm','define Ohm'], \
-    ['DMM','define DMM'], \
-    ['silicon','define silicon'], \
     ['dopant','define dopant'], \
     ['orbital','define orbital'], \
     ['Centigrus','A real unit for measuring temperature. Invented at WCJC by Blake Folmar.'], \
@@ -74,65 +29,6 @@ word_list = ['coulomb', 'A fundamental unit of electrical charge, and is also th
     ['Intel','define Intel'], \
     ['Fairchild','define Fairchild'], \
     ['TI','define TI'], \
-    ['National','define National Semiconductor'], \
-    ['semiconductor','define semiconductor'], \
-    ['germanium','define germanium'], \
-    ['Linux','define Linux'], \
-    ['Microsoft','define Microsoft'], \
-    ['Windows','define Windows'], \
-    ['Macintosh','define Macintosh'], \
-    ['Apple','define Apple'], \
-    ['BSD','define BSD'], \
-    ['Unix','define Unix'], \
-    ['Posix','define Posix'], \
-    ['oscillator','define oscillator'], \
-    ['triangle','define triangle'], \
-    ['square','define square'], \
-    ['breakdown','define breakdown'], \
-    ['Tesla','define Tesla'], \
-    ['Gauss','define Gauss'], \
-    ['plot','define plot'], \
-    ['data','define data'], \
-    ['network','define network'], \
-    ['Maxwell','define Maxwell'], \
-    ['feedback','define feedback'], \
-    ['feedforward','define feedforward'], \
-    ['PID','define PID'], \
-    ['Lucent','define Lucent'], \
-    ['BellLabs','define BellLabs'], \
-    ['Alcatel','define Alcatel'], \
-    ['HP','define HP'], \
-    ['HewlettPackard','define HewlettPackard'], \
-    ['Agilent','define Agilent'], \
-    ['oscilloscope','define oscilloscope'], \
-    ['snubber','define snubber'], \
-    ['RAM','define RAM'], \
-    ['ROM','define ROM'], \
-    ['Flash','define Flash'], \
-    ['serial','define serial'], \
-    ['USB','define USB'], \
-    ['IEEE','define IEEE'], \
-    ['Firewire','define Firewire'], \
-    ['parallel','define parallel'], \
-    ['Chua','define Chua'], \
-    ['PROM','define PROM'], \
-    ['EPROM','define EPROM'], \
-    ['EEPROM','define EEPROM'], \
-    ['FPGA','define FPGA'], \
-    ['VHDL','define VHDL'], \
-    ['Verilog','define Verilog'], \
-    ['PLD','define PLD'], \
-    ['CPLD','define CPLD'], \
-    ['linear','define linear'], \
-    ['logarithmic','define logarithmic'], \
-    ['exponential','define exponential'], \
-    ['polynomial','define polynomial'], \
-    ['RMS','define RMS'], \
-    ['Power','define Power'], \
-    ['invert','define invert'], \
-    ['phase','define phase'], \
-    ['agc','define agc'], \
-    ['control','define control'], \
     ['logic','define logic'], \
     ['microprocessor','define microprocessor'], \
     ['microcontroller','define microcontroller'], \
@@ -236,3 +132,121 @@ word_list = ['coulomb', 'A fundamental unit of electrical charge, and is also th
     ['Kelvin','An absolute, thermodynamic temperature scale using as its null point absolute zero.'], \
     ['Oersted','Danish physicist who discovered that a steady electric current creates a magnetic field around it.'], \
     ['FCC','Spectrum regulator.']
+    ['National', 'American semiconductor manufacturer which specialized in analog devices and subsystems'], \ #also mention Bob Pease
+    ['semiconductor', 'A substance that has conductivity between an insulator and most metals'], \
+    ['germanium', 'Atomic number 32'], \
+    ['Linux', 'A Unix-like and mostly POSIX-compliant computer operating system'], \
+    ['Microsoft', 'A multinational corporation that develops, manufactures, licenses, and sells computer software'], \
+    ['Windows', 'is a series of graphical interface operating systems developed, marketed, and sold by Microsoft'], \
+    ['Macintosh', 'A series of personal computers (PCs) designed, developed, and marketed by Apple Inc'], \
+    ['Apple', 'American multinational corporation that designs develops and sells consumer electronics'], \
+    ['BSD', 'A Unix operating system derivative developed and distributed by the CSRG.'], \
+    ['Unix', 'A multitasking, multiuser computer operating system that exists in many variants. Invented at AT&T'], \
+    ['Posix', 'Standards specified by the IEEE for maintaining compatibility between operating systems.'], \
+    ['Oscillator', 'A electronic circuit that produces a periodic, repeating electronic signal.'], \
+    ['triangle', 'A polygon with three edges and three vertices. Also a name for a waveform.'], \
+    ['square', 'A rectangular quadrilateral'], \
+    ['breakdown', 'The abrupt failure of an insulator or insulating medium to restrict the flow of current.'], \
+    ['filter', 'Blocks out any unwanted frequency in a circuit'], \
+    ['sawtooth', 'a non sine wave that resembles the teeth of a saw'], \
+    ['sine', 'A waveform that is curvy'], \
+    ['waveform', 'T representation of a signal as a plot of amplitude versus time'], \
+    ['current', 'The flow of electric charge'] \
+    ['Kirchhoff', 'Inventor of current and voltage law'] \
+    ['Faraday', 'The si unit of capacitance is named after him'] \
+    ['farad', 'si unit of capacitance'] \
+    ['Norton', 'developed nortons theorem'] \
+    ['Thevenin', 'developed Thevenins theorem'] \
+    ['cable', 'Two or more wires side by side'] \
+    ['coaxial', 'A cable with a center conductor, insulation, wire braid around that, and more insulation'] \
+    ['Ohm', 'Unit of resistance'] \
+    ['DMM', 'A typically handheld electronic measurement device.'] \
+    ['silicon', 'The basis for most semiconductors.'] \
+    ['Tesla', 'The wickedest electrical genius evar.'], \
+    ['Gauss', 'is the cgs unit of measurement of a magnetic field'], \
+    ['Plot', 'is a graph of the transfer function of a linear, time-invariant system versus frequency'], \
+    ['Data', 'the quantities, characters, or symbols on which operations are performed by a computer, being stored and transmitted in the form of electrical signals and recorded on magnetic, optical, or mechanical recording media'], \
+    ['Network', 'a group or system of interconnected people or things'], \
+    ['Maxwell', 'a unit of magnetic flux in the centimeter-gram-second system, equal to that induced through one square centimeter by a perpendicular magnetic field of one gauss'], \
+    ['Feedback', 'the return to the input of a part of the output of a machine, system, or process'], \
+    ['Feedforward' , 'an element or pathway within a control system which passes a controlling signal from a source in its external environment, often a command signal from an external operator, to a load elsewhere in its external environment'], \
+    ['PID', 'a proportional-intergral-dervative controller is a control loop feedback machanism'], \
+    ['Lucent', 'glowing with or giving off light'], \
+    ['BellLabs', 'is the research and development subsidiary of Alcatel Lucent'], \
+    ['Alcatel', 'is a french global telecommunications equipment company'], \
+    ['HP', 'Hewlett Packard is an American multinational information technology corporation which provides hardware, software and services to everyone'], \
+    ['HewlettPackard', 'Hewlett-Packard Company or HP is an American multinational information technology corporation'], \
+    ['Agilent', 'is an American company that designs and manufactures electronic and bio-analytical measurement instruments and equipment for measurement and evaluation'], \
+    ['triac', 'an electronic component that can conduct current in either direction when it is triggered '], \
+    ['FET', 'a transistor that uses an electric field to control the shape and hence the conductivity of a channel of one type of charge carrier in a semiconductor material.'],\
+    ['MOSFET', 'a type of transistor used for amplifying or switching electronic signals.'], \
+    ['NPN', 'NPN is one of the two types of bipolar transistors, consisting of a layer of P-doped semiconductor between two N-doped layers.'],\
+    ['PNP', 'The other type of BJT is the PNP, consisting of a layer of N-doped semiconductor between two layers of P-doped material.'],\
+    ['spectrum', 'a condition that is not limited to a specific set of values but can vary infinitely within a continuum. '],\
+    ['spectra', 'conditions or values that vary over a continuum.'], \
+    ['Fourier', 'a French mathematician and physicist best known for initiating the investigation of Fourier series and their applications to problems of heat transfer and vibrations.'],\
+    ['potentiometer', 'a pot, is a three-terminal resistor with a sliding or rotating contact that forms an adjustable voltage divider.'],\
+    ['digital', 'signals by discrete bands of analog levels, rather than by a continuous range.'], \
+    ['analog', 'electronic systems with a continuously variable signal, in contrast to digital electronics where signals usually take only two different levels.'],\
+    ['gain', 'a measure of the ability of a circuit to increase the power or amplitude of a signal from the input to the output by adding energy converted from some power supply to the signal.'],\
+    ['bridge', 'a type of electrical circuit in which two circuit branches are "bridged" by a third branch connected between the first two branches at some intermediate point along them. '],\
+    ['rectifier', 'A rectifier is an electrical device that converts alternating current (AC), which periodically reverses direction, to direct current , which flows in only one direction.'],\
+    ['microprocessor', 'Incorporates the functions of a computer central processing unit (CPU) on a single integrated circuit (IC).'], \
+    ['microcontroller', ' is a small computer on a single integrated circuit containing a processor core, memory, and programmable input/output peripherals.']
+    ['architecture', 'refers to the identification of a systems physical components and their interrelationships'], \
+    ['ALU or Arithmetic Logic Unit', 'Is a digital circuit that performs integer arithmetic and logical operations.'], \
+    ['Register', 'Are data storage devices that are more sophisticated than latches. Is a group of binary cells suitable for holding binary information.'], \
+    ['Word', 'Is basically a fixed-sized group of digits (binary or decimal) that are handled as a unit by the instruction set or the hardware of the processor.'], \
+    ['Bitmask', 'Makes use of the fact that binary numbers are made up of 1s and 0s, each digit in a binary number being equivalent to one bit'], \
+    ['Variable', 's a storage location and an associated symbolic name (an identifier) which contains some known or unknown quantity or information'], \
+    ['Static', 'displayed when no transmission signal is obtained by the antenna receiver of television sets and other display devices.'], \
+    ['Dynamic', ' Of data storage, processing, or programming affected by the passage of time or the presence or absence of power'], \
+    ['Data', 'a set of values of qualitative or quantitative variables; restated, pieces of data are individual pieces of Information'], \
+    ['Type', 'is a classification identifying one of various of data, such as real, integer or Boolean'], \
+    ['Compiler', 'Source code written in a programming language (the source language) into another computer language (the target language, often having a binary form known as object code)'], \
+    ['Assembler', 'is a program which creates object code by translating combinations of mnemonics and syntax for operations and addressing modes into their numerical equivalents.'], \
+    ['VHDL',' Hardware description language used in electronic design automation and general purpose parallel programming language.'], \
+    ['Verilog',' Hardware description language.'], \
+    ['PLD','Build re-configurable digital circuits with it.'], \
+    ['CPLD','Build re-configurable digital circuits with it.'], \
+    ['linear','Able to be represented by a straight line on a graph'], \
+    ['logarithmic','Constructed so that successive points along an axis, or graduations that are an equal distance apart, represent values that are in an equal ratio'], \
+    ['exponential',' Becoming more and more rapid'], \
+    ['polynomial','An expression of more than two algebraic terms'], \
+    ['RMS','.707'], \
+    ['Power','Energy that is produced by electrical means and used to operate a device'], \
+    ['invert','To put in opposite arrangement'], \
+    ['phase','The relationship in time between successive states or cycles'], \
+    ['agc','The average or peak output signal level is used to adjust the signal output to a suitable level'], \
+    ['control','A means of limiting or regulating something'], \
+    ['logic','A system or set of principles underlying the arrangements of elements in a computer or device so as to perform a specified task'], \
+    ['oscilloscope', 'a type of electronic test equipment that allows observation of constantly varying signal voltages, usually as a two-dimensional plot of one or more signals as a function of time'], \
+    ['snubber', 'a device to suppress voltage transients in electrical systems'], \
+    ['RAM', 'Daft Punk named an album after this kind of memory.'], \
+    ['ROM', 'Memory that cannot form new memories.'], \
+    ['Flash', 'an electronic non-volatile computer storage medium that can be electrically erased and reprogrammed'] \
+    ['serial', 'any medium issued in successive parts bearing numerical or chronological designation and intended to be continued indefinitely'], \
+    ['USB', 'an industry standard that defines the cables, connectors and communication protocols used in a bus for connection, communication, and power supply between computers and electronic devices'], \
+    ['IEEE', ' a professional association formed in 1963 from the amalgamation of the American Institute of Electrical Engineers and the Institute of Radio Engineers'], \
+    ['Firewire', 'an interface standard for a serial bus for high-speed communications and isochronous real-time data transfer, a.k.a IEEE 1394'], \
+    ['parallel', 'Side by side'], \
+    ['Chua', 'Memristor predictor.'], \
+    ['PROM', 'a form of digital memory where the setting of each bit is locked by a fuse or antifuse'], \
+    ['EPROM', 'a type of memory chip that retains its data when its power supply is switched off'], \
+    ['EEPROM', 'a type of non-volatile memory used in computers and other electronic devices to store small amounts of data that must be saved when power is removed'], \
+    ['FPGA', 'an integrated circuit designed to be configured by a customer or a designer after manufacturing, field-programmable'], \
+    ['capacitance', 'The ability of a body to store an electrical charge'], \
+    ['impedance', 'The measure of the opposition that a circuit presents to a current when a voltage is applied'], \
+    ['resistance', 'A measure of the degree to which conductor opposes an electric current through that conductor'], \
+    ['resistor', 'A passive two-terminal electrical component that implements electrical resistance as a circuit element'], \
+    ['capacitor', 'A passive two-terminal electrical component used to store energy electrostatically in an electric field'], \
+    ['inductor', 'Also called a coil or reactor, is a passive two-terminal electrical component which resists changes in electric current passing through it'], \
+    ['kilo', 'Denoting multiplication by one thousand'], \
+    ['Mega', 'A factor of one million'], \
+    ['Giga', 'A factor of a billion'], \
+    ['Tera', 'Multiplication of a power of twelve'], \
+    ['Peta', 'Multiplication of a power of fifteen'], \
+    ['bit', 'The basic unit of information in computing and digital communications'], \
+    ['byte', 'A unit of digital information in computing and telecommunications that most commonly consists of eight bits'], \
+    ['diode', 'A two-terminal electronic component with asymmetric conductance; it has low (ideally zero) resistance to current in one direction, and high (ideally infinite) resistance in the other'], \
+    ['LED', 'A two-lead semiconductor light source'], \
