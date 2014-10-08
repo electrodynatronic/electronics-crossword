0af4b25110ea78de60b9f379ee7c692144d21559
electrodynatronic <electrodynatronic@gmail.com>
2014-10-08 09:39:55 -0500

    Create wordlist_huge.py
    Made one huge separate file for the word and hint list. -CN

---
+++ wordlist_huge.py
@@ -0,0 +1,238 @@
+word_list = ['coulomb', 'A fundamental unit of electrical charge, and is also the SI derived unit of electric charge (symbol: Q or q). It is equal to the charge of approximately 6.241×1018 electrons.'], \
+    ['Ampere', 'A measure of the amount of electric charge passing a point in an electric circuit per unit time. 1 coulomb per second.'], \
+    ['Watt', 'A joule per second.'], \
+    ['milli', 'A prefix in the metric system denoting a factor of one thousandth (10-3).'], \
+    ['micro', 'A prefix in the metric system denoting a factor of one millionth (10-6).'], \
+    ['nano', 'A prefix in the metric system denoting a factor of one billionth (10-9).'], \
+    ['pico', 'A prefix in the metric system denoting a factor of one trillionth (10-12).'], \
+    ['femto', 'A prefix in the metric system denoting a factor of one quadrillionth (10-15).'], \
+    ['atto', 'A prefix in the metric system denoting a factor of one quintillionth (10-18).'], \
+    ['zepto', 'A prefix in the metric system denoting a factor of one sextillionth (10-21).'], \
+    ['yocto', 'A prefix in the metric system denoting a factor of one septillionth (10-24).'], \
+    ['kilo', 'define kilo'], \
+    ['Mega', 'define Mega'], \
+    ['Giga', 'define Giga'], \
+    ['Tera', 'define Tera'], \
+    ['Peta', 'define Peta'], \
+    ['electron', 'define electron'], \
+    ['binary', 'two states'], \
+    ['transistor', 'A semiconductor device used to amplify and switch electronic signals and electrical power.'], \
+    ['amplifier', 'An electronic device that increase the power of a signal.'], \
+    ['inductance', 'he property of a conductor by which a change in current flowing through it "induces" (creates) a voltage (electromotive force) in both the conductor itself and in any nearby conductors.'], \
+    ['capacitance', 'define capacitance'], \
+    ['impedance', 'define impedance'], \
+    ['resistance', 'define resistance'], \
+    ['resistor', 'define resistor'], \
+    ['capacitor', 'define capacitor'], \
+    ['inductor', 'define inductor'], \
+    ['bit', 'define bit'], \
+    ['byte', 'define byte'], \
+    ['diode', 'define diode'], \
+    ['LED', 'define LED'], \
+    ['triac', 'define triac'], \
+    ['FET', 'define FET'], \
+    ['MOSFET', 'define MOSFET'], \
+    ['NPN', 'define NPN'], \
+    ['PNP', 'define PNP'], \
+    ['spectrum', 'define spectrum'], \
+    ['spectra', 'define spectra'], \
+    ['Fourier', 'define Fourier'], \
+    ['FFT', 'define FFT'], \
+    ['potentiometer', 'define potentiometer'], \
+    ['digital', 'define digital'], \
+    ['analog', 'define analog'], \
+    ['gain','define gain'], \
+    ['bridge','define bridge'], \
+    ['rectifier','define rectifier'], \
+    ['filter','define filter'], \
+    ['sawtooth','define sawtooth'], \
+    ['sine','define sine'], \
+    ['waveform','define waveform'], \
+    ['current','define current'], \
+    ['Kirchhoff','define Kirchhoff'], \
+    ['Faraday','define Faraday'], \
+    ['farad','define farad'], \
+    ['Norton','define Norton'], \
+    ['Thevenin','define Thevenin'], \
+    ['cable','define cable'], \
+    ['coaxial','define coaxial'], \
+    ['Ohm','define Ohm'], \
+    ['DMM','define DMM'], \
+    ['silicon','define silicon'], \
+    ['dopant','define dopant'], \
+    ['orbital','define orbital'], \
+    ['Centigrus','A real unit for measuring temperature. Invented at WCJC by Blake Folmar.'], \
+    ['arduino','define arduino'], \
+    ['Atmel','define Atmel'], \
+    ['AVR','define AVR'], \
+    ['PIC','define PIC'], \
+    ['picaxe','define picaxe'], \
+    ['ARM','define ARM'], \
+    ['RISC','define RISC'], \
+    ['MIPS','define MIPS'], \
+    ['x86','define x86'], \
+    ['Intel','define Intel'], \
+    ['Fairchild','define Fairchild'], \
+    ['TI','define TI'], \
+    ['National','define National Semiconductor'], \
+    ['semiconductor','define semiconductor'], \
+    ['germanium','define germanium'], \
+    ['Linux','define Linux'], \
+    ['Microsoft','define Microsoft'], \
+    ['Windows','define Windows'], \
+    ['Macintosh','define Macintosh'], \
+    ['Apple','define Apple'], \
+    ['BSD','define BSD'], \
+    ['Unix','define Unix'], \
+    ['Posix','define Posix'], \
+    ['oscillator','define oscillator'], \
+    ['triangle','define triangle'], \
+    ['square','define square'], \
+    ['breakdown','define breakdown'], \
+    ['Tesla','define Tesla'], \
+    ['Gauss','define Gauss'], \
+    ['plot','define plot'], \
+    ['data','define data'], \
+    ['network','define network'], \
+    ['Maxwell','define Maxwell'], \
+    ['feedback','define feedback'], \
+    ['feedforward','define feedforward'], \
+    ['PID','define PID'], \
+    ['Lucent','define Lucent'], \
+    ['BellLabs','define BellLabs'], \
+    ['Alcatel','define Alcatel'], \
+    ['HP','define HP'], \
+    ['HewlettPackard','define HewlettPackard'], \
+    ['Agilent','define Agilent'], \
+    ['oscilloscope','define oscilloscope'], \
+    ['snubber','define snubber'], \
+    ['RAM','define RAM'], \
+    ['ROM','define ROM'], \
+    ['Flash','define Flash'], \
+    ['serial','define serial'], \
+    ['USB','define USB'], \
+    ['IEEE','define IEEE'], \
+    ['Firewire','define Firewire'], \
+    ['parallel','define parallel'], \
+    ['Chua','define Chua'], \
+    ['PROM','define PROM'], \
+    ['EPROM','define EPROM'], \
+    ['EEPROM','define EEPROM'], \
+    ['FPGA','define FPGA'], \
+    ['VHDL','define VHDL'], \
+    ['Verilog','define Verilog'], \
+    ['PLD','define PLD'], \
+    ['CPLD','define CPLD'], \
+    ['linear','define linear'], \
+    ['logarithmic','define logarithmic'], \
+    ['exponential','define exponential'], \
+    ['polynomial','define polynomial'], \
+    ['RMS','define RMS'], \
+    ['Power','define Power'], \
+    ['invert','define invert'], \
+    ['phase','define phase'], \
+    ['agc','define agc'], \
+    ['control','define control'], \
+    ['logic','define logic'], \
+    ['microprocessor','define microprocessor'], \
+    ['microcontroller','define microcontroller'], \
+    ['architecture','define architecture'], \
+    ['alu','define alu'], \
+    ['register','define register'], \
+    ['word','define word'], \
+    ['bitmask','define bitmask'], \
+    ['variable','define variable'], \
+    ['static','define static'], \
+    ['dynamic','define dynamic'], \
+    ['polar','define polar'], \
+    ['data','define data'], \
+    ['type','define type'], \
+    ['compiler','define compiler'], \
+    ['assembler','define assembler'], \
+    ['opcode','define opcode'], \
+    ['memory','define memory'], \
+    ['PLC','define PLC'], \
+    ['relay','define relay'], \
+    ['SSR','define SSR'], \
+    ['diac','define diac'], \
+    ['IGBT','define IGBT'], \
+    ['macro','define macro'], \
+    ['ASCII','define ASCII'], \
+    ['unicode','define unicode'], \
+    ['encode','define encode'], \
+    ['protocol','define protocol'], \
+    ['circuit','define circuit'], \
+    ['PCB','define PCB'], \
+    ['transformer','define transformer'], \
+    ['flux','define flux'], \
+    ['lumen','define lumen'], \
+    ['proportional','define proportional'], \
+    ['Hitachi','define Hitachi'], \
+    ['decibel','define decibel'], \
+    ['bel','define bel'], \
+    ['encrypt','The process of encoding messages or information in such a way that only authorized parties can read it.'], \
+    ['ethernet','IEEE 802.3, RJ-45, Gigabit, etc.'], \
+    ['antenna','A transducer for sending or receiving waves.'], \
+    ['transducer','A device that converts a signal in one form of energy to another form of energy.'], \
+    ['login','signon'], \
+    ['password','Authentication token.'], \
+    ['URL','TLA for web address. See RFC 3986'], \
+    ['internet','Not a truck. A series of tubes.'], \
+    ['DARPA','Military agency responsible for cat videos.'], \
+    ['satellite','A thing that kind of hangs around. There are a bunch of them overhead.'], \
+    ['apogee','The big Apsis. Not perihelion, the other one.'], \
+    ['perigee','The little Apsis. Not aphelion, the other one.'], \
+    ['GPS','Satnav'], \
+    ['latency','A measure of the time delay experienced by a system.'], \
+    ['timeout','A delay.'], \
+    ['band','A small section of the radio spectrum.'], \
+    ['bandwidth','A measure of the width of a range of frequencies, measured in hertz.'], \
+    ['axial','Two leads, one on each end, pointing in opposite directions.'], \
+    ['radial','Two leads, both on the same end.'], \
+    ['sink','A heat ___.'], \
+    ['NO','"Make" contacts. Form A contacts.'], \
+    ['NC','"Break" contacts. Form B contacts.'], \
+    ['JFET','Electric charge flows through a semiconducting channel between source and drain terminals.'], \
+    ['Ground','The reference point in an electrical circuit from which voltages are measured, a common return path for electric current, or a direct physical connection to the Earth.'], \
+    ['Vcc','Collector supply line voltage in a common NPN circuit.'], \
+    ['TTL','A mechanism that limits the lifespan of data in a computer or network. A popular family of integrated-circuit digital logic.'], \
+    ['SCPI','A standard for syntax and commands to use in controlling programmable test and measurement devices. Think *IDN?'], \
+    ['Volt','The derived unit for electric potential'], \
+    ['Voltage','The electric potential difference between two points.'], \
+    ['loop','A hardware or software method which feeds a received signal or data back to the sender. A pseudo-device that makes a file accessible as a block device.'], \
+    ['sensor','A device that detects events or changes in quantities and provides a corresponding output, generally as an electrical or optical signal.'], \
+    ['thermistor','A temperature-dependent resistor.'], \
+    ['photocell','A kind of light-dependent resistor.'], \
+    ['phototransistor','A light-sensitive transistor.'], \
+    ['optocoupler','A component that transfers electrical signals between two isolated circuits by using light.'], \
+    ['optoisolator','A component that transfers electrical signals between two isolated circuits by using light.'], \
+    ['software','Instructions that direct a computer processor to perform specific operations.'], \
+    ['firmware','Embedded systems have this. Embedded engieers write it. It usually is not installable by the end user.'], \
+    ['automation','Robotic labor'], \
+    ['DCS','A very broad term used to monitor and control distributed equipment in process plants and industrial processes.'], \
+    ['LCD','TN, IPS, S-IPS, AFFS, etc. Uses polarized light. '], \
+    ['OLED', 'Totally natural emissive electroluminescent.'], \
+    ['VFD','AC motor power supply.'], \
+    ['transmitter','Makes waves, best paired with an antenna'], \
+    ['receiver','Converts the information carried by radio waves to a usable form.'], \
+    ['duplex','Two-way'], \
+    ['ECC','Successor to Hamming code, self fixing.'], \
+    ['SRAM','A type of semiconductor memory that uses bistable latching circuitry to store each bit. '], \
+    ['SDRAM','Synchronous, and dynamic.'], \
+    ['DRAM','A type of memory that stores each bit of data in a separate capacitor. '], \
+    ['DDR','Twice as fast.'], \
+    ['NVRAM','random-access memory that retains its information when power is turned off '], \
+    ['RTC','A computer clock (most often in the form of an integrated circuit) that keeps track of the current time, usually contains a long-life battery.'], \
+    ['SPICE','An open source analog electronic circuit simulator'], \
+    ['SPI','SCLK, MISO, MOSI, SS.'], \
+    ['Actel','FPGA Manufacturer.'], \
+    ['Altera','FPGA Manufacturer.'], \
+    ['Xilinx','FPGA Manufacturer.'], \
+    ['RaspberryPi','Small $35.00 computer.'], \
+    ['Android','Paranoid _______'], \
+    ['Fahrenheit','A temperature scale based on one proposed in 1724 by the German physicist, after whom the scale is named.'], \
+    ['Celcius','A scale and unit of measurement for temperature with 0 degrees as the freezing point of water.'], \
+    ['Centigrade','A scale and unit of measurement for temperature with 0 degrees as the freezing point of water.'], \
+    ['Kelvin','An absolute, thermodynamic temperature scale using as its null point absolute zero.'], \
+    ['Oersted','Danish physicist who discovered that a steady electric current creates a magnetic field around it.'], \
+    ['FCC','Spectrum regulator.']
